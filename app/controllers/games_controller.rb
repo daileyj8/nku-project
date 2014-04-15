cab674cd7e3f7878e8d4400d7073ffd0898cccd2
John Dailey <daileyj8@nku.edu>
2014-04-15 02:45:58 +0000

    Added admin to User and some permissions to views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,28 +1,47 @@
 class GamesController < ApplicationController
   
   def new
+    unless session[:user_id] != nil
+      flash[:notice] = "You must log in!"
+      redirect_to new_session_path
+      return
+    end
+    @current=get_current
     @game= Game.new
+    unless @current.admin
+      redirect_to '/games', notice: "You do not have permission to do that!"
+      return
+    end
   end
   
   def create
-    @game = Game.new
-    @game.name= params[:game][:name]
+    @current=get_current
+    if @current.admin
+      @game = Game.new
+      @game.name= params[:game][:name]
+      @game.genre= params[:game][:genre]
+      @game.num_in_stock= params[:game][:num_in_stock]
+      @game.rating= params[:game][:rating]
+      @game.description= params[:game][:description]
+      @game.on_hand= params[:game][:on_hand]
     
-    @game.genre= params[:game][:genre]
-    @game.num_in_stock= params[:game][:num_in_stock]
-    @game.rating= params[:game][:rating]
-    @game.description= params[:game][:description]
-    @game.on_hand= params[:game][:on_hand]
-    
-    if @game.save
-      redirect_to '/games/new'
+      if @game.save
+        redirect_to '/games/new'
       
+      else
+        render 'new'
+      end
     else
-      render 'new'
+      redirect_to '/games', notice: "You do not have permission to do that!"
     end
   end
   
   def show
+    unless session[:user_id] != nil
+      flash[:notice] = "You must log in"
+      redirect_to new_session_path
+      return
+    end
     @game= Game.find(params[:id])
    
   end
@@ -36,6 +55,11 @@ class GamesController < ApplicationController
   end 
 
   def soon
+    unless session[:user_id] != nil
+      flash[:notice] = "You must log in!"
+      redirect_to new_session_path
+      return
+    end
   end
   
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412133840) do
+ActiveRecord::Schema.define(version: 20140415020347) do
 
   create_table "games", force: true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140412133840) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
 end
